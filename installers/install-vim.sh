90dbb60ac5a95ee1c067e5297583379855e68455
Julian A. Garcia de Rubertis <jagadrub@gmail.com>
2021-07-26 00:03:28 -0300

    Organizing files

--- installers/install-vim.sh
+++ installers/install-vim.sh
@@ -3,6 +3,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2> /dev/null || (cd ~/.vim/bundle/Vundle.vim ; git pull)
 git clone https://github.com/ycm-core/YouCompleteMe ~/.vim/bundle/YouCompleteMe 2> /dev/null || (cd ~/.vim/bundle/YouCompleteMe ; git pull)
 vim +PluginInstall +qall
-python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer --system-libclang
-vim +CocInstall coc-json coc-tsserver coc-json coc-html coc-css coc-python coc-go +qall
-
+python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer --system-libclang --gocode-completer
