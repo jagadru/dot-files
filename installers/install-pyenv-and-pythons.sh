104508c021104827fb22c1f2509430764444a8f0
Julian A. Garcia de Rubertis <jagadrub@gmail.com>
2021-07-29 22:47:00 -0300

    Added brewfile and git plugins

--- installers/install-pyenv-and-pythons.sh
+++ installers/install-pyenv-and-pythons.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo -e 'export PYTHON_CONFIGURE_OPTS="--enable-shared"' >> ~/.profile
+if ! grep -q PYTHON_CONFIGURE_OPTS ~/.profile ; then echo -e 'export PYTHON_CONFIGURE_OPTS="--enable-shared"' >> ~/.profile; fi
 source ~/.profile         # this might take a few minutes the first time
-pyenv -s install 2.7.17
-pyenv -s install 3.7.4
+pyenv install 2.7.17
+pyenv install 3.7.4
 pyenv virtualenv 2.7.17 dev-2.7.17
 pyenv virtualenv 3.7.4 dev-3.7.4
 
--- installers/install-vim.sh
+++ installers/install-vim.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2> /dev/null || (cd ~/.vim/bundle/Vundle.vim ; git pull)
-git clone https://github.com/ycm-core/YouCompleteMe ~/.vim/bundle/YouCompleteMe 2> /dev/null || (cd ~/.vim/bundle/YouCompleteMe ; git pull)
 vim +PluginInstall +qall
 python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer --system-libclang --gocode-completer
